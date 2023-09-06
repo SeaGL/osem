347c929973fc906994b2661752aeb1c4a1c6f33c
Andrew Kvalheim <andrew@kvalhe.im>
2023-09-05 18:32:33 -0700

    Work around titusfortner/webdrivers#‌254

--- spec/support/external_request.rb
+++ spec/support/external_request.rb
@@ -5,6 +5,7 @@ require 'webmock/rspec'
 driver_urls = Webdrivers::Common.subclasses.map do |driver|
   Addressable::URI.parse(driver.base_url).host
 end
+driver_urls += %w[googlechromelabs.github.io edgedl.me.gvt1.com storage.googleapis.com] # Workaround for titusfortner/webdrivers#254
 WebMock.disable_net_connect!(allow_localhost: true, allow: [*driver_urls, /stripe.com/])
 
 RSpec.configure do |config|
